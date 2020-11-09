090651a0a66e66109da4b085e45a99285a06713d
Simon Field <sfield66@gmail.com>
2020-11-10 00:17:33 +1100

    Update model.ts
    Started docs for model:
    
    * added function get();
    
    `need to find out how to highlight text > (bold and links) for starters`

--- server/src/docs/model.ts
+++ server/src/docs/model.ts
@@ -3,10 +3,9 @@ export const model = [
 		label: 'get',
 		kind: 'function',
 		data: 1,
-		doc: 'this is the long doc',
-		shortDoc: 'short doc',
-		signature: '($what, $ever)'
-
+		doc: 'Trongate function > get();\r\n\r\nget rows from db as an array\r\n\r\n$data[\'rows\'] = $this->model->get();\r\n\r\nIf you don\'t add params - returns all rows as an array reading the module name from the URL\r\n\r\n@param `$order_by` - Order by\r\n[optional] - default `id`\r\n\r\n@param `$target_tbl` - Target table\r\n[optional] - default module name\r\n\r\n@param `$limit` - Limit rows\r\n[optional]\r\n\r\n@param `$offset`\r\n[optional]\r\n\r\n@returns an array of rows',
+		shortDoc: 'get([$order_by][, $target_tbl][, $limit][, $offset]);',
+		signature: '([$order_by][, $target_tbl][, $limit][, $offset]);'
 	},
 	{
 		label: 'get_where_custom',
