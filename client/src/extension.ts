2334a2c6f58659a4bac2b3b2d87ef71bac144210
jakecastelli <959672929@qq.com>
2020-11-30 22:07:10 +1030

    Included protected function and added a fix
    * We included protected functiont to show in autocomplete eg: function
      _hi() {}
    
    * Fixed the bug in #9

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -60,16 +60,30 @@ export function activate(context: ExtensionContext) {
 		clientOptions
 	);
 
-	// Start the client. This will also launch the server
-	const fsPath =workspace.workspaceFolders[0].uri.fsPath
-	const isTrongateProject = checkIsTrongateProject(fsPath)
-	if(isTrongateProject) {
-		client.registerProposedFeatures();
-		client.start();
-		client.onReady().then(() => {
-			console.log('ok, it is on')
-		});
+	try {
+		// // // if (workspace.workspaceFolders[0].uri?.fsPath) return
+		// // // Start the client. This will also launch the server
+		// // console.log(workspace.workspaceFolders[0].uri?.fsPath)
+
+		// // const fsPath = workspace.workspaceFolders[0].uri.fsPath
+		// // const isTrongateProject = checkIsTrongateProject(fsPath)
+		// if (isTrongateProject) {
+			client.registerProposedFeatures();
+			client.start();
+			client.onReady().then(() => {
+				console.log('ok, it is on')
+
+				const fsPath = workspace.workspaceFolders[0].uri.fsPath
+				const isTrongateProject = checkIsTrongateProject(fsPath)
+				if (!isTrongateProject) {
+					client.stop();
+				}
+			});
+		// }
+	} catch (error) {
+		console.log(error)	
 	}
+	
 }
 
 export function deactivate(): Thenable<void> | undefined {
